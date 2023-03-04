acc493d038fc212432e4b02e8b22f43de61f137f
bhargav <bhargav@bhargavs-MacBook-Air.local>
2023-03-04 13:08:34 +0530

    Add Tests

--- tests/lib/rules/check-function-was-called.js
+++ tests/lib/rules/check-function-was-called.js
@@ -11,7 +11,6 @@
 const rule = require("../../../lib/rules/check-function-was-called"),
   RuleTester = require("eslint").RuleTester;
 
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
@@ -19,13 +18,15 @@ const rule = require("../../../lib/rules/check-function-was-called"),
 const ruleTester = new RuleTester();
 ruleTester.run("check-function-was-called", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      code: "function add(){return 1;} add()",
+    },
   ],
 
   invalid: [
     {
       code: "function add(){return 1;}",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      errors: [{ message: `function was declared but never called` }],
     },
   ],
 });
